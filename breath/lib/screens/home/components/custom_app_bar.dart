bde023940c1c7758fb91a846a5566ab76917ef26
HyeongJun Kim <khj029956@naver.com>
2025-03-05 22:58:14 +0900

    Style: 상단 AppBar 아이콘 -> image

--- breath/lib/screens/home/components/custom_app_bar.dart
+++ breath/lib/screens/home/components/custom_app_bar.dart
@@ -9,25 +9,41 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
-      title: IconButton(
-        icon: Icon(Icons.account_circle, color: Colors.white),
-        onPressed: () {
-          // 프로필 화면 이동
+      title: GestureDetector(
+        onTap: () {
+          // 프로필 버튼
         },
+        child: Image.asset(
+          "assets/icons/home/profile_icon.png",
+          width: 40,
+          height: 40,
+        ),
       ),
       actions: [
-        IconButton(
-          icon: Icon(Icons.add, color: Colors.white),
-          onPressed: () {
-            // 공황 기록 추가
+        GestureDetector(
+          onTap: () {
+            // 공황 기록 버튼
           },
+          child: Image.asset(
+            "assets/icons/home/more_icon.png",
+            width: 32,
+            height: 32,
+            color: Colors.white,
+          ),
         ),
-        IconButton(
-          icon: Icon(Icons.notifications_none, color: Colors.white),
-          onPressed: () {
-            // 알림 기능
+        SizedBox(width: 20),
+        GestureDetector(
+          onTap: () {
+            // 알림 버튼
           },
+          child: Image.asset(
+            "assets/icons/home/alert_icon.png",
+            width: 32,
+            height: 32,
+            color: Colors.white,
+          ),
         ),
+        SizedBox(width: 14),
       ],
     );
   }
